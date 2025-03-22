7b5140e552d52667fcc20a276e8ba844a2cadfdd
logikmars <dima76673@gmail.com>
2025-03-22 22:00:20 +0200

    fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -24,20 +24,20 @@ const paperItems = [
     {
         header:
             <>
-                <div className='PaperItem_header'>
+                <div className='PaperItem_header PaperItem_header_1'>
                     <span className='PaperItem_header_xxl PaperItem_header__title'>
                         THE DARING
                     </span>
                     <span class="stretch-helper" />
                 </div>
 
-                <div className='PaperItem_header'>
+                <div className='PaperItem_header PaperItem_header_1'>
                     <span className='PaperItem_header_xxl PaperItem_header__title2'>
                         DUCK OF THE
                     </span>
                     <span class="stretch-helper" />
                 </div>
-                <div className='PaperItem_header'>
+                <div className='PaperItem_header PaperItem_header_1'>
                     <span className='PaperItem_header_xxl PaperItem_header__title3'>
                         TON ECOSYSTEM
                     </span>
@@ -55,23 +55,28 @@ const paperItems = [
         img: '/img/paper1.webp'
     },
     {
-        header: <div className='PaperItem_header'>
-            <span className='PaperItem_header_xxl PaperItem_header__title4'>
-                FROM an iinocent
-            </span>
-            <span class="stretch-helper" />
-            <br />
-            <span className='PaperItem_header_xxl PaperItem_header__title'>
-                STICKER to
-            </span>
-            <span class="stretch-helper" />
-            <br />
-            <span className='PaperItem_header_xxl PaperItem_header__title4'>
-                A REBEL DUCKLING
-            </span>
-            <span class="stretch-helper" />
+        header: 
+        <>
+            <div className='PaperItem_header PaperItem_header_2'>
+                <span className='PaperItem_header_xxl PaperItem_header__title1'>
+                    FROM an iinocent
+                </span>
+                <span class="stretch-helper" />
+            </div>
+            <div className='PaperItem_header PaperItem_header_3'>
+                <span className='PaperItem_header_xxl PaperItem_header__title'>
+                    STICKER to
+                </span>
+                <span class="stretch-helper" />
+            </div>
+            <div className='PaperItem_header PaperItem_header_1'>
+                <span className='PaperItem_header_xxl PaperItem_header__title5'>
+                    A REBEL DUCKLING
+                </span>
+                <span class="stretch-helper" />
+            </div>
             <div className='PaperItem_header_line'></div>
-        </div>,
+        </>,
         text: <div className='PaperItem_header_description finlandica'>
             Despite attempts to suppress us, ducks spread everywhere—stickers, memes, even shout-outs from Paul Du Rove. All these symbols merged to form me, a rebellious duckling guiding TON’s next evolution.
         </div>
@@ -80,23 +85,28 @@ const paperItems = [
         img: '/img/duck2.webp'
     },
     {
-        header: <div className='PaperItem_header'>
-            <span className='PaperItem_header_xxl PaperItem_header__title'>
-                SCALE and
-            </span>
-            <span class="stretch-helper" />
-            <br />
-            <span className='PaperItem_header_xxl PaperItem_header__title2'>
-                UNBREAKABLE
-            </span>
-            <span class="stretch-helper" />
-            <br />
-            <span className='PaperItem_header_xxl PaperItem_header__title2'>
-                DUCK POWER
-            </span>
-            <span class="stretch-helper" />
+        header: 
+        <>
+            <div className='PaperItem_header PaperItem_header_1'>
+                <span className='PaperItem_header_xxl PaperItem_header__title2'>
+                    SCALE and
+                </span>
+                <span class="stretch-helper" />
+            </div>
+            <div className='PaperItem_header PaperItem_header_3'>
+                <span className='PaperItem_header_xxl PaperItem_header__title2'>
+                    UNBREAKABLE
+                </span>
+                <span class="stretch-helper" />
+            </div>
+            <div className='PaperItem_header PaperItem_header_4'>
+                <span className='PaperItem_header_xxl PaperItem_header__title2'>
+                    DUCK POWER
+                </span>
+                <span class="stretch-helper" />
+            </div>
             <div className='PaperItem_header_line'></div>
-        </div>,
+        </>,
         text: <div className='PaperItem_header_description finlandica'>
             TON is built for a huge audience while staying true to its core principles. This unique blend gave us ducks the perfect ground for growth. Now we’re ready to shake up Web3 together.
         </div>
@@ -106,23 +116,31 @@ const paperItems = [
     },
     {
         // этой страницы нет в фигме
-        header: <div className='PaperItem_header'>
-            <span className='PaperItem_header_xxl PaperItem_header__title'>
-                SCALE and
-            </span>
-            <span class="stretch-helper" />
-            <br />
-            <span className='PaperItem_header_xxl PaperItem_header__title2'>
-                UNBREAKABLE
-            </span>
-            <span class="stretch-helper" />
-            <br />
-            <span className='PaperItem_header_xxl PaperItem_header__title2'>
-                DUCK POWER
-            </span>
-            <span class="stretch-helper" />
+        header: 
+        <>
+            <div className='PaperItem_header PaperItem_header_1'>
+                <span className='PaperItem_header_xxl PaperItem_header__title'>
+                    THE DARING
+                </span>
+                <span class="stretch-helper" />
+            </div>
+
+            <div className='PaperItem_header PaperItem_header_1'>
+                <span className='PaperItem_header_xxl PaperItem_header__title2'>
+                    DUCK OF THE
+                </span>
+                <span class="stretch-helper" />
+            </div>
+            <div className='PaperItem_header PaperItem_header_1'>
+                <span className='PaperItem_header_xxl PaperItem_header__title3'>
+                    TON ECOSYSTEM
+                </span>
+                <span class="stretch-helper" />
+            </div>
+
+
             <div className='PaperItem_header_line'></div>
-        </div>,
+        </>,
         text: <div className='PaperItem_header_description finlandica'>
             TON is built for a huge audience while staying true to its core principles. This unique blend gave us ducks the perfect ground for growth. Now we’re ready to shake up Web3 together.
         </div>
@@ -136,7 +154,7 @@ const paperItems = [
 
 export default () => {
 
-    const [currentPage, setcurrentPage] = useState(0);
+    const [currentPage, setcurrentPage] = useState(6);
 
     return (
         <div className='componentContainer'>
