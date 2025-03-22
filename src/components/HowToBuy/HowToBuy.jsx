436dba81fd491e570d483de1f8351a12112372b2
ivandalechenko <ivandalechenko@gmail.com>
2025-03-22 23:04:10 +0200

    fix

--- src/components/HowToBuy/HowToBuy.jsx
+++ src/components/HowToBuy/HowToBuy.jsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import './HowToBuy.scss';
 import HowToBuyItem from './HowToBuyItem/HowToBuyItem';
 export default () => {
-    
+
     const HowToBuyItems = [
         {
             img: "/img/htb.png",
@@ -13,27 +14,42 @@ export default () => {
             title: "Swap TON for $DON",
             description: "Exchange TON for $DON with no taxes.You don’t need to change the slippage, but if the transaction doesn’t go through — increase it to 1–3%."
         },
-        // {
-        //     img: "/img/htb.png",
-        //     title: "WHERE TO BUY TON?",
-        //     description: "Buy TON with a card directly in Tonkeeper or TonSpace Or via exchanges: OKX, Bybit, KuCoin, EXMO, HTX, and others Also available on: Uniswap (ETH) and PancakeSwap (BSC)Full list — on CoinMarketCap"
-        // },
-        // {
-        //     img: "/img/htb.png",
-        //     title: "HOW TO BRIDGE TO TON?",
-        //     description: "Bridge from any network to TON: [bridge.ton.org](https://bridge.ton.org [RocketX](https://www.rocketx.exchange [ChangeNOW](https://changenow.io) or via a CEX that supports TON withdrawals"
-        // },
+        {
+            img: "/img/htb.png",
+            title: "WHERE TO BUY TON?",
+            description: "Buy TON with a card directly in Tonkeeper or TonSpace Or via exchanges: OKX, Bybit, KuCoin, EXMO, HTX, and others Also available on: Uniswap (ETH) and PancakeSwap (BSC)Full list — on CoinMarketCap"
+        },
+        {
+            img: "/img/htb.png",
+            title: "HOW TO BRIDGE TO TON?",
+            description: "Bridge from any network to TON: [bridge.ton.org](https://bridge.ton.org [RocketX](https://www.rocketx.exchange [ChangeNOW](https://changenow.io) or via a CEX that supports TON withdrawals"
+        },
     ]
-    
+
+
+    const [currentSlide, setcurrentSlide] = useState(1);
+
+    // uasdasdasdasd
+    // asdasd
+
     return (
-    <div className='componentContainer'>
-        <div className='HowToBuy'>
-            <p className='HowToBuy__title obrazec textShadow'>HOW TO BUY</p>
-            <div className='HowToBuy__wrapper'>
-                {HowToBuyItems.map((el, index) => (
-                    <HowToBuyItem img={el.img} index={index + 1} title={el.title} description={el.description}/>
-                ))}
+        <div className='componentContainer'>
+            <div className='HowToBuy'>
+                <p className='HowToBuy__title obrazec textShadow' onClick={() => {
+                    setcurrentSlide(prev => (prev + 1) < 5 ? prev + 1 : 1)
+                }}>HOW TO BUY</p>
+                <div className='HowToBuy__wrapper'>
+                    {HowToBuyItems.map((el, index) => (
+                        <HowToBuyItem
+                            img={el.img}
+                            index={index + 1}
+                            title={el.title}
+                            description={el.description}
+                            currentSlide={currentSlide}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-)}
+    )
+}
--- src/components/HowToBuy/HowToBuyItem/HowToBuyItem.jsx
+++ src/components/HowToBuy/HowToBuyItem/HowToBuyItem.jsx
@@ -1,13 +1,22 @@
 import './HowToBuyItem.scss';
-export default ({ img, index, title, description}) => {return (
-    <div className='HowToBuyItem free_img'>
-        <div className='HowToBuyItem__img'>
-            <img src={img} alt="" className='HowToBuyItem__img_img'/>
-            <div className='HowToBuyItem__index free_img'><p className='HowToBuyItem__index_text textShadow outfit'>{index}</p></div>
+export default ({ img, index, title, description, currentSlide }) => {
+    return (
+        <div className='HowToBuyItem_wrapper free_img' style={{
+            transform: `translate(${(currentSlide - index) * 600 * -1}px,0px) `,
+            opacity: (currentSlide === index) ? 1 : .5
+        }}>
+            <div className='HowToBuyItem'>
+                <div className='HowToBuyItem__img'>
+                    <img src={img} alt="" className='HowToBuyItem__img_img' />
+                    <div className='HowToBuyItem__index free_img'>
+                        <p className='HowToBuyItem__index_text textShadow outfit'>{index}</p>
+                    </div>
+                </div>
+                <div className='HowToBuyItem__description'>
+                    <p className='HowToBuyItem__description_title outfit textShadow'>{title}</p>
+                    <p className='HowToBuyItem__description_description finlandica'>{description}</p>
+                </div>
+            </div>
         </div>
-        <div className='HowToBuyItem__description'>
-            <p className='HowToBuyItem__description_title outfit textShadow'>{title}</p>
-            <p className='HowToBuyItem__description_description finlandica'>{description}</p>
-        </div>
-    </div>
-)}
+    )
+}
